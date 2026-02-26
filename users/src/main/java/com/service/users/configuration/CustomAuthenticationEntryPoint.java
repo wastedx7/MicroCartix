96c0198926a2868497ea4987405f5681a5b75df8
wastedx7 <wastedx7@gmail.com>
2026-02-26 20:46:49 +0530

    auth entry point

---
+++ users/src/main/java/com/service/users/configuration/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,31 @@
+package com.service.users.configuration;
+
+import java.io.IOException;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        AuthenticationException authException
+    ) throws IOException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        response.getWriter().write(
+            "{\"authenticated\": false, \"error\": \"Unauthorized\", \"message\": \"" 
+            + authException.getMessage() + "\"}"
+        );
+    }
+}
