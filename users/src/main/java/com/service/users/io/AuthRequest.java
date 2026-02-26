a7c37bab87e8db442636336315a705bad3660870
wastedx7 <wastedx7@gmail.com>
2026-02-26 20:54:23 +0530

    auth req

--- users/src/main/java/com/service/users/io/AuthRequest.java
+++ users/src/main/java/com/service/users/io/AuthRequest.java
@@ -1,5 +1,16 @@
 package com.service.users.io;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthRequest {
     
+    private String email;
+    private String password;
 }
