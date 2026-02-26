8d665474d3439d08bf4c80734b340d820556c656
wastedx7 <wastedx7@gmail.com>
2026-02-26 20:55:34 +0530

    auth response

---
+++ users/src/main/java/com/service/users/io/AuthResponse.java
@@ -0,0 +1,12 @@
+package com.service.users.io;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthResponse {
+    
+    private String email;
+    private String token;
+}
