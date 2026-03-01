df68af70b901a0322c719ecda8cd6cf22a2a1c8f
wastedx7 <wastedx7@gmail.com>
2026-03-01 13:06:29 +0530

    tweak

--- users/src/main/java/com/service/users/controllers/UserController.java
+++ users/src/main/java/com/service/users/controllers/UserController.java
@@ -6,9 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.service.users.io.RegisterRequest;
 import com.service.users.io.RegisterResponse;
-import com.service.users.repositories.UserRepository;
 import com.service.users.service.UserService;
-
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -16,13 +14,9 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
     
     private final UserService userService;
-    private final UserRepository userRepository;
 
     @PostMapping("/register")
     public RegisterResponse register(@RequestBody RegisterRequest request){
-        if (!userRepository.existsByEmail(request.getEmail())){
-            return userService.register(request);
-        }
-        throw new RuntimeException("user already exists for this email");
+        return userService.register(request);
     }
 }
