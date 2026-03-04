c32143e41b3b70395474f8839fd19b4eabd4b088
wastedx7 <wastedx7@gmail.com>
2026-03-04 08:30:46 +0530

    comment added for products fetch

--- users/src/main/java/com/service/users/controllers/AdminController.java
+++ users/src/main/java/com/service/users/controllers/AdminController.java
@@ -2,11 +2,17 @@ package com.service.users.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.service.users.repositories.AdminRepository;
+
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class AdminController {
     
-    
+    private final AdminRepository adminRepository;
+
+    public void getAllProducts(){
+        /* this is a future function to return all available products */
+    }
 }
