26e5feab45459923ad0b51b02d16b2bbf3bb657f
wastedx7 <wastedx7@gmail.com>
2026-03-04 08:28:26 +0530

    admin api initailised

---
+++ users/src/main/java/com/service/users/controllers/AdminController.java
@@ -0,0 +1,12 @@
+package com.service.users.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class AdminController {
+    
+    
+}
