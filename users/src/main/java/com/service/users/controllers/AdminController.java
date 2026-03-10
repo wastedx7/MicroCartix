bcc3d05de128ea03bd5e235415f457412c1c5e8e
wastedx7 <wastedx7@gmail.com>
2026-03-10 20:17:24 +0530

    done for today

--- users/src/main/java/com/service/users/controllers/AdminController.java
+++ users/src/main/java/com/service/users/controllers/AdminController.java
@@ -3,8 +3,6 @@ package com.service.users.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.service.users.repositories.AdminRepository;
-
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -12,8 +10,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AdminController {
     
-    private final AdminRepository adminRepository;
-
     public void getAllProducts(){
         /* this is a future function to return all available products */
     }
