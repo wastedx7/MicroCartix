abf2d3dbe738568f3374eefece93ba01d8b4af86
wastedx7 <wastedx7@gmail.com>
2026-03-10 18:03:34 +0530

    added gitignore cuz this .vscode lowkey haunting me

--- users/src/main/java/com/service/users/model/AdminEntity.java
+++ users/src/main/java/com/service/users/model/AdminEntity.java
@@ -24,6 +24,8 @@ public class AdminEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String userId;
     private String email;
     private String password;
     
